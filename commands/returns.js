8d9babfa826ea3c740380b746afa233d776f70f1
Sonduong <Sonduong@PROMETHEUS>
2020-04-27 11:38:47 -0500

    I HAVE SLAYED THE EMOJI DEMON

--- commands/returns.js
+++ commands/returns.js
@@ -2,24 +2,26 @@ const gSheet = require('../lib/gSheet.js')
 const bestPrices = require('../commands/best-price.js')
 
 module.exports = {
-        name: 'returns',
-        description: 'Reports potential returns on turnips bought and sold this week.',
+    name: 'returns',
+    description: 'Reports potential returns on turnips bought and sold this week.',
         
-        async execute(message) {
+    async execute(message) {
 
-// Thanks Tom: https://blog.abelotech.com/posts/number-currency-formatting-javascript/
-function formatNumber(num) {
-    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
-}
+        // Thanks Tom: https://blog.abelotech.com/posts/number-currency-formatting-javascript/
+        function formatNumber(num) {
+            return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+        }
 
-function filterLedgerRows(rows, username){
-    const now = new Date();
-    const today = new Date(now.toDateString());
-    const days_since_sunday = today.getDay()
-    let start_of_week = new Date(today-days_since_sunday*24*3_600_000);
+        function filterLedgerRows(rows, username){
+            const now = new Date();
+            const today = new Date(now.toDateString());
+            const days_since_sunday = today.getDay()
+            let start_of_week = new Date(today-days_since_sunday*24*3_600_000);
 
-    return rows.filter(row => row.buyer == username && new Date(row.date) >= start_of_week)
-}
+            return rows.filter(row => row.buyer == username && new Date(row.date) >= start_of_week)
+        }
+
+        emojis = message.client.emojis.cache.reduce((d,e)=>(d[e.name]=e,d),{})
         const ledgerSheet = await gSheet.getSheet('ledger');
 
         let ledger = await ledgerSheet.getRows().then(
@@ -31,7 +33,7 @@ function filterLedgerRows(rows, username){
             return total + parseInt(next.quantity) * parseInt(next.price)
         }, 0)
         inventory = ledger.reduce( (total, next) => {return total + parseInt(next.quantity)}, 0)
-        parts = [`You bought ${inventory} <:turnip:699780899738484797> for 🔔${formatNumber(baseValue)}`]
+        parts = [`You bought ${inventory} ${emojis['turnip']} for 🔔${formatNumber(baseValue)}`]
 
         if (!(best_today == null)){
             bestValueToday = inventory*best_today.price
@@ -42,7 +44,7 @@ function filterLedgerRows(rows, username){
             parts.push(`Best this week at 🔔${formatNumber(best_week.price)} for 🔔${formatNumber(bestValueWeek)} at ${((bestValueWeek/baseValue)*100-100).toFixed(2)}% RoI`)
         }
         if (!best_today && !best_week){
-            parts.push('No prices to compare to <:isgraphs:691420747465228309>')
+            parts.push(`No prices to compare to ${emojis['isgraphs']}`)
         }
 
         message.channel.send(parts.join(`\n`));
